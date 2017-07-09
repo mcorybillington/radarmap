eda0d8ef9b4dd68f66c1a60bacfa762539fd2b72
Cory Billington <billingtonm@rodbourn.com>
2017-07-09 15:11:13 -0400

    WUGround_ApiKeyProvider added to store api key outside of source control

---
+++ radarMap/WUGround_ApiKeyProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace radarMap
+{
+    public static class WUGround_ApiKeyProvider
+    {
+        public static string ApiKey { get; set; } = "your key here";
+    }
+}
